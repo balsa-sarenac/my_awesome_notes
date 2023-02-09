c1c6e66c5c3d0cde7233c501307a0555d7217ee2
Balša Šarenac <balsa.smi15@gmail.com>
2023-02-10 00:46:55 +0100

    refactor(core): change app name

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:my_awesome_notes/views/login_view.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
-    title: 'Flutter Demo',
+    title: 'My Awesome Notes',
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
