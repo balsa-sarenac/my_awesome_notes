2e53d0b150d862175bf48acdc7a1c8273d3119d2
Balša Šarenac <balsa.smi15@gmail.com>
2023-02-10 18:37:08 +0100

    fix(core): HomePage displaying logic
    Display correct view if user is logged in or not.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -34,11 +36,16 @@ class HomePage extends StatelessWidget {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
               final user = FirebaseAuth.instance.currentUser;
-              if (user?.emailVerified ?? false) {
-                return const Center(child: Text('Done'));
+              if (user != null) {
+                if (user.emailVerified) {
+                  log('Email is verified');
+                } else {
+                  return const VerifyEmailView();
+                }
               } else {
-                return const VerifyEmailView();
+                return const LoginView();
               }
+              return const Text('Done');
             default:
               return const CircularProgressIndicator();
           }
